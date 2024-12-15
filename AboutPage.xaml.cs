59e0f2429553901b13498515c1921a953aa99f82
BiancaElenaC <biasami@yahoo.com>
2024-12-15 11:04:22 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CiorteaTrifBiancaElenaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CiorteaTrifBiancaElenaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
